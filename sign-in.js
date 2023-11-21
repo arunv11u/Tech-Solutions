79e94bf281d7e6f8ef13259d8bea1a3b0177b504
Arun Varadharajalu <arunvaradharajalu@gmail.com>
2023-11-21 13:29:42 -0500

    Add footer to reset password and sign-in pages.

--- sign-in.js
+++ sign-in.js
@@ -2,6 +2,16 @@ $(function () {
 	const login = $("#login-btn");
 
 	login.click(() => {
-		console.log("Login form submitted :", $("#username").val(), $("#password").val());
+		const validCredentials = {
+			username: "arun",
+			password: "Arun@123"
+		};
+		const username = $("#username").val();
+		const password = $("#password").val();
+
+		if(username != validCredentials.username || password != validCredentials.password)
+			return alert("Invalid Credentials");
+
+		return alert("Successful login");
 	});
 });
